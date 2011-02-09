0094bc0366cf9258ae9aae0cca0b0e296b4557d8
Tim Jones <github@roastedamoeba.com>
2011-02-10 00:20:45 +0800

    New SharpDX binaries Attempt to use hardware device before falling back to WARP

--- src/DotWarp/Util/DeviceUtility.cs
+++ src/DotWarp/Util/DeviceUtility.cs
@@ -1,12 +1,35 @@
+using SharpDX;
 using SharpDX.Direct3D10;
 
 namespace DotWarp.Util
 {
 	public static class DeviceUtility
 	{
+		private static bool _noHardware;
+
 		public static Device CreateDevice()
 		{
-			return new Device1(DriverType.Warp, DeviceCreationFlags.None, FeatureLevel.Level_10_1);
+			// If we have previously found that there's no compatible hardware device,
+			// go straight to creating a WARP device.
+			if (_noHardware)
+				return CreateDevice(DriverType.Warp);
+
+			// First attempt to create a hardware device. This will fail if the host computer
+			// does not have a graphics card that supports feature level 10.1.
+			try
+			{
+				return CreateDevice(DriverType.Hardware);
+			}
+			catch (SharpDXException)
+			{
+				_noHardware = true;
+				return CreateDevice(DriverType.Warp);
+			}
+		}
+
+		private static Device1 CreateDevice(DriverType driverType)
+		{
+			return new Device1(driverType, DeviceCreationFlags.None, FeatureLevel.Level_10_1);
 		}
 	}
 }
