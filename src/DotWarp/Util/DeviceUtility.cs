10b68f4e6276d4d8574da405f04036c66c7b1d9b
Tim Jones <tim@timjones.tw>
2012-02-16 15:54:10 +0800

    Always use Warp device

--- src/DotWarp/Util/DeviceUtility.cs
+++ src/DotWarp/Util/DeviceUtility.cs
@@ -6,31 +6,9 @@ namespace DotWarp.Util
 {
 	public static class DeviceUtility
 	{
-		private static bool _noHardware;
-
 		public static Device CreateDevice()
 		{
-			// If we have previously found that there's no compatible hardware device,
-			// go straight to creating a WARP device.
-			if (_noHardware)
-				return CreateDevice(DriverType.Warp);
-
-			// First attempt to create a hardware device. This will fail if the host computer
-			// does not have a graphics card that supports feature level 10.1.
-			try
-			{
-				return CreateDevice(DriverType.Hardware);
-			}
-			catch (SharpDXException)
-			{
-				_noHardware = true;
-				return CreateDevice(DriverType.Warp);
-			}
-		}
-
-		private static Device CreateDevice(DriverType driverType)
-		{
-			return new Device(driverType, DeviceCreationFlags.None, FeatureLevel.Level_10_1);
+            return new Device(DriverType.Warp, DeviceCreationFlags.None, FeatureLevel.Level_10_1);
 		}
 	}
 }
